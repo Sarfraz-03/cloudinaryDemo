d463543bdf57ddcac5382674fc4bb1a227282efb
sarfraz <sarfrazmohammed181@gmail.com>
2026-04-05 11:13:40 +0530

    updated backend which is required for the deployment

--- imageApp/client/src/App.jsx
+++ imageApp/client/src/App.jsx
@@ -16,7 +16,7 @@ function App() {
   useEffect(() => {
     const loadUsers = async () => {
       try {
-        const res = await axios.get("http://localhost:5000/api/users");
+        const res = await axios.get(`${import.meta.env.VITE_API_URL}/api/users`)
         setUsers(res.data);
       } catch (err) {
         console.error("Failed to fetch users", err);
