d827fe3201d19c85388f331aa963486847955f0b
sarfraz <sarfrazmohammed181@gmail.com>
2026-04-11 15:03:50 +0530

    edited

--- imageApp/server/server.js
+++ imageApp/server/server.js
@@ -1,19 +1,17 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const cors = require("cors");
 const PORT = process.env.PORT || 5000;
 
 const cors = require("cors");
 
-app.use(cors({
-  origin: "*",
-}));
 
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "*",
+}));
 app.use(express.json());
 
 const userRoutes = require("./routes/userRoutes");
